176993d0786d6e9fb4685d21786a8da3173463b3
pilna <pilna@outlook.fr>
2021-12-09 12:18:54 +0100

    add msg fruite

---
+++ README.md
@@ -0,0 +1,72 @@
+# Sprint Cryptographie
+
+nom de code : Lauriot
+
+## chiffrement historique:
+
+Premier chiffrement: Vigenere
+Source utilisée pour le décodé: (decode)[https://www.dcode.fr/vigenere-cipher]
+
+```
+Subject: T'es on ???
+From: Alice
+To: Lauriot
+Date: Thu, 09 Dec 2021 08:15:00 +0100
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="===============1133673754986327044=="
+
+--===============1133673754986327044==
+Content-Type: text/plain; charset="utf-8"
+Content-Transfer-Encoding: 8bit
+
+Salut,
+
+Tu sais qu'il y a SPRINT de crypto ce matin ?
+
+On t'attend en E09, magne-toi ! Y a des tokens t trouver !
+
+De chez toi, le plus simple est de prendre le tramway A
+jusqu't l'arrxt %lparcfloral:Parc Floral%n, je te mets un
+plan en pixce-jointe, ce sera pas du luxe...
+
+Allez, t tout de suite !
+Alice
+
+--===============1133673754986327044==
+Content-Type: image/png
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename="tram.png"
+MIME-Version: 1.0
+```
+
+Image parc floral:
+```
+QMEGGGMS, QMEGGGMS, 
+```
+
+
+Un message Fruité
+```
+correspondance = {
+    "🍎": "a",
+    "🥝": "e",
+    "🍊": "t",
+    "🥭": "s",
+    "🍈": "p",
+    "🍑": "h",
+    "🍐": "r",
+    "🧄": "o",
+    "🍓": "y",
+    "🥦": "l",
+    "🌽": "i",
+    "🥬": "f",
+    "🥑": "c",
+    "🍉": "u",
+    "🥔": "g",
+    "🍅": "v",
+    "🥕": "d",
+    "🌶": "n",
+    "🍒": "m",
+    "🥥": "b"
+}
+```
---
+++ tram.py
@@ -0,0 +1,90 @@
+from typing import List, Union, Any
+from Crypto.Util.strxor import strxor
+from PIL import Image
+import numpy as np
+
+class Color:
+
+    def __init__(self, red: int, green: int, blue: int, alpha: int = 0) -> None:
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+
+    def __eq__(self, target: Any) -> bool:
+        if not isinstance(target, Color):
+            return False
+        return self.red == target.red and self.green == target.green and self.blue == target.blue
+
+    def __str__(self) -> str:
+        return f"(r:{self.red}, g:{self.green}, b:{self.blue}, a:{self.alpha})"
+
+class Pixel:
+
+    def __init__(self, color: Color, x: int, y: int) -> None:
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f'color : {self.color}, pos: ({self.x}, {self.y})'
+
+
+def get_pixel_of_image(image: List[List[List[int]]]) -> List[Pixel]:
+    pixels = []
+
+    for y, row in enumerate(image):
+        for x, color_of_pixel in enumerate(row):
+            pixels.append(Pixel(Color(*color_of_pixel), x, y))
+
+    return pixels
+
+def get_all_rgb_component(image: List[List[List[int]]]) -> List[int]:
+    red_pixels, green_pixels, blue_pixels = [], [], []
+
+    for row in image:
+        for red, green, blue in row:
+            red_pixels.append(red)
+            green_pixels.append(green)
+            blue_pixels.append(blue)
+
+    return red_pixels, green_pixels, blue_pixels
+
+def get_lower_bits(bytes: bytes) -> int:
+    return [byte & 1 for byte in bytes]
+
+image = Image.open("tram.png")
+image = np.array(image)
+pixels = get_pixel_of_image(image)
+red_pixels, green_pixels, blue_pixels = get_all_rgb_component(image)
+hash_red = bytes(red_pixels)
+hash_green = bytes(green_pixels)
+hash_blue = bytes(blue_pixels)
+rgb_pixels = []
+
+for pixel in pixels:
+    rgb_pixels.append(pixel.color.red)
+    rgb_pixels.append(pixel.color.green)
+    rgb_pixels.append(pixel.color.blue)
+
+rgb_pixels = bytes(rgb_pixels)
+lower = get_lower_bits(rgb_pixels)
+lower = "".join(str(l) for l in lower)
+
+binaries = []
+
+for i in range(0, len(lower), 8):
+    binaries.append(lower[i:i+8].zfill(8))
+
+for binary in binaries:
+    if binary != "11111111":
+        print(chr(int(binary, 2)), end="")
+
+
+
+
+
+
+
+
+
