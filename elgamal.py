fb9458aea35c53ad7f9b1253b6c0caac899081b3
Jean-Daniel de Ambrogi <jddea@icloud.com>
2021-12-10 15:57:03 +0100

    sauver

--- elgamal.py
+++ elgamal.py
@@ -28,34 +28,34 @@ def exponentionRapide(c1, s, mod):
         else:
             return (c1 * exponentionRapide(res, s // 2, mod)) % mod
 
+if __name__ == "__main__":
+    with open("part3/elgamal/y") as f:
+        y = f.read()
+    with open("part3/elgamal/n") as f:
+        n = f.read()
+    with open("part3/elgamal/ey") as f:
+        ey = f.read()
+    with open("part3/elgamal/es") as f:
+        es = f.read()
+    with open("part3/elgamal/c1") as f:
+        c1 = f.read()
+    with open("part3/elgamal/c2") as f:
+        c2 = f.read()
+    with open("part3/elgamal/ec1") as f:
+        ec1 = f.read()
+    with open("part3/elgamal/ec2") as f:
+        ec2 = f.read()
 
-with open("part3/elgamal/y") as f:
-    y = f.read()
-with open("part3/elgamal/n") as f:
-    n = f.read()
-with open("part3/elgamal/ey") as f:
-    ey = f.read()
-with open("part3/elgamal/es") as f:
-    es = f.read()
-with open("part3/elgamal/c1") as f:
-    c1 = f.read()
-with open("part3/elgamal/c2") as f:
-    c2 = f.read()
-with open("part3/elgamal/ec1") as f:
-    ec1 = f.read()
-with open("part3/elgamal/ec2") as f:
-    ec2 = f.read()
+    ec2 = int(ec2, 2)
+    ec1 = int(ec1, 2)
+    es = int(es, 2)
+    n = int(n, 2)
 
-ec2 = int(ec2, 2)
-ec1 = int(ec1, 2)
-es = int(es, 2)
-n = int(n, 2)
-
-print(es)
-#decrypt = ec2 / ec1**(es)
-decryptN = exponentionRapide(ec1,es,n)
-print(ec2/decryptN)
-"""decryptique = decrypt([ec2],ec1,es,n)
-print(decryptique)
-print(len(decryptique))
-"""
+    print(es)
+    #decrypt = ec2 / ec1**(es)
+    decryptN = exponentionRapide(ec1,es,n)
+    print(ec2/decryptN)
+    """decryptique = decrypt([ec2],ec1,es,n)
+    print(decryptique)
+    print(len(decryptique))
+    """
